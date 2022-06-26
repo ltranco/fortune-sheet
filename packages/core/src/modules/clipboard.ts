6b546d02f5afd52116e987413fd063f399c6aca3
Long Tran <ltran.co8@gmail.com>
2022-06-26 03:03:19 -0700

    fixed clipboard issue on Safari

--- packages/core/src/modules/clipboard.ts
+++ packages/core/src/modules/clipboard.ts
@@ -1,25 +1,6 @@
 export default class clipboard {
   static writeHtml(str: string) {
-    try {
-      let ele = document.getElementById("fortune-copy-content");
-      if (!ele) {
-        ele = document.createElement("div");
-        ele.setAttribute("contentEditable", "true");
-        ele.id = "fortune-copy-content";
-        ele.style.position = "fixed";
-        ele.style.height = "0";
-        ele.style.width = "0";
-        ele.style.left = "-10000px";
-        document.querySelector(".fortune-container")?.append(ele);
-      }
-      ele.style.display = "block";
-      ele.innerHTML = str;
-      ele.focus({ preventScroll: true });
-      document.execCommand("selectAll");
-      document.execCommand("copy");
-      ele.style.display = "none";
-    } catch (e) {
-      console.error(e);
-    }
+    navigator.clipboard.writeText(str);
+    // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
   }
 }
